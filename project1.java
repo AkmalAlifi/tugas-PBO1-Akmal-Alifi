dbb29a38dd91718283b151868938f87cabe62869
AkmalAlifi <akmal.alifi.aa@gmail.com>
2021-04-02 04:18:28 -0700

    file baru project1

--- project1.java
+++ project1.java
@@ -1,5 +1,5 @@
-public class project01{
-public Static void main (String[] args) {
+public class project1{
+public static void main (String[] args) {
 int sum1 = 100 + 50;
 int sum2 = sum1 + 250;
 int sum3 = sum2 + sum2;
